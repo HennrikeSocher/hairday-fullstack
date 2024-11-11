6a39c9e2f93140892271fdd900311c32f325ed48
HennrikeSocher <socherhennrike@gmail.com>
2024-11-11 18:00:15 -0300

    finish package module

---
+++ src/main.js
@@ -0,0 +1,10 @@
+"use strict"
+
+// Configuração do day js
+import "./libs/dayjs.js"
+
+//Css importa todos arquivos para load, sendo assim removemos o index.css e tiramos o import de dentro do html
+// graças ao web pack não precisamos fazer essas coisas pois ele transforma tudo em bundler
+import "./styles/global.css"
+import "./styles/form.css"
+import "./styles/schedule.css"
